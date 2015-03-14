dc92c9e25bacd578460f92f98c4233e5c36bae7c
TheGameFather <f15gdsy@gmail.com>
2015-03-14 22:37:09 +0800

    - Added BTCheckDataChanged

---
+++ Core/Conditional/BTCheckDataChanged.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace BT {
+
+	public class BTCheckDataChanged<T> : BTConditional {
+		private string _readDataName;
+		private int _readDataId;
+		private T _previousData;
+
+
+		public BTCheckDataChanged (string readDataName) {
+			_readDataName = readDataName;
+		}
+
+		public override void Activate (BTDatabase database) {
+			base.Activate (database);
+
+			_readDataId = database.GetDataId(_readDataName);
+			_previousData = database.GetData<T>(_readDataId);
+		}
+
+		public override bool Check () {
+			T data = _database.GetData<T>(_readDataId);
+			if (!_previousData.Equals(data)) {
+				_previousData = data;
+				return true;
+			}
+			return false;
+		}
+	}
+
+}
